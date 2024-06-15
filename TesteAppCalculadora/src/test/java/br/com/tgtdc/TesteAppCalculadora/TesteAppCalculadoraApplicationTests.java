c8b87df6c6788b3f006ef0e6ff5bf2bceb88e1ea
Thiago Castro <thiagogyorgy.castro@gmail.com>
2024-06-15 09:08:46 -0300

    Iniciando projeto Spring Calculadora

---
+++ TesteAppCalculadora/src/test/java/br/com/tgtdc/TesteAppCalculadora/TesteAppCalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tgtdc.TesteAppCalculadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteAppCalculadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
