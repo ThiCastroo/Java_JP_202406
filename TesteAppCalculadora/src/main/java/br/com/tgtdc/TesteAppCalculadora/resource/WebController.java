b0f93e7e089bfebd2f4cb4ae857d151a6bf4b365
Thiago Castro <thiagogyorgy.castro@gmail.com>
2024-06-15 09:43:05 -0300

    Java Web com Spring, fazendo uma calculadora

--- TesteAppCalculadora/src/main/java/br/com/tgtdc/TesteAppCalculadora/resource/WebController.java
+++ TesteAppCalculadora/src/main/java/br/com/tgtdc/TesteAppCalculadora/resource/WebController.java
@@ -3,12 +3,28 @@ package br.com.tgtdc.TesteAppCalculadora.resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebController {
 
 	@GetMapping("/")
 	public String showForm(Model model) {
+		return "index";
+	}
+	
+	@PostMapping("/")
+	public String handleFormSubmission(
+			@RequestParam String modelOperacoes,
+			@RequestParam String valor01,
+			@RequestParam String valor02,
+			Model model) {
+		
+		System.out.println("Model Operações: " + modelOperacoes + ", " +
+						   "Valor 01: " + valor01 + ", " +
+						   "Valor 02: " + valor02);
+		
 		return "index";
 	}
 	
