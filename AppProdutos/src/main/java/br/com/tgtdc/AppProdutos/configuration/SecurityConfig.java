7a554e3249be0e07a6ef6ffaacba0885f4003094
Thiago Castro <thiagogyorgy.castro@gmail.com>
2024-06-22 16:36:19 -0300

    Utilizando JWT Token e fazendo configurações no app

--- AppProdutos/src/main/java/br/com/tgtdc/AppProdutos/configuration/SecurityConfig.java
+++ AppProdutos/src/main/java/br/com/tgtdc/AppProdutos/configuration/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -24,8 +25,16 @@ public class SecurityConfig {
 							"/swagger-ui.html",
 							"/swagger-resources/**",
 							"webjars/**"
-							).permitAll().anyRequest().authenticated()
+							).permitAll().anyRequest().authenticated())
+			.addFilterBefore(
+					jwtTokenFilter(), 
+					UsernamePasswordAuthenticationFilter.class
 					);
 		return http.build();
+	}
+	
+	@Bean
+	public JwtTokenFilter jwtTokenFilter() {
+		return new JwtTokenFilter();
 	}
 }
