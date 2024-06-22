9e34be21a3c4b5d41f4b10af4f894ad2ea69a0d2
Thiago Castro <thiagogyorgy.castro@gmail.com>
2024-06-22 08:46:38 -0300

    Interface

--- AppProdutos/src/main/java/br/com/tgtdc/AppProdutos/service/interfaces/EstoqueServiceInterface.java
+++ AppProdutos/src/main/java/br/com/tgtdc/AppProdutos/service/interfaces/EstoqueServiceInterface.java
@@ -1,5 +1,16 @@
 package br.com.tgtdc.AppProdutos.service.interfaces;
 
-public interface EstoqueServiceInterface {
+import java.util.List;
+import java.util.Optional;
 
+import br.com.tgtdc.AppProdutos.model.Estoque;
+
+public interface EstoqueServiceInterface {
+	
+	Estoque save(Estoque estoque);
+	Optional<Estoque> findById(Long id);
+	List<Estoque> findAll();
+	Estoque update(Estoque estoque);
+	void delete (Long id);
+	
 }
